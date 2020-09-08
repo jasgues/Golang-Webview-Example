f1d88d1719230526eed4805ada315185ea9ea423
J <webmanyak@gmail.com>
2020-09-08 16:29:08 -0700

    Initial commit

---
+++ webview.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/zserge/webview"
+
+func main() {	
+		webview.Open("Deneme",
+		"https://kodcuzade.com", 800, 600, true)			
+	
+/* Bir sürü fonksiyonu olan bir kütüphane. İncelemenizi öneririm: github.com/webview/webview  */
+}
